445293888599bec7dcc94305c1e71ff4c33542eb
erik-sandoval <erik.sandoval@icloud.com>
2019-04-22 15:25:29 -0700

    finished Header Component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import './Header.css';
+import './Header.scss';
+
+const HeaderTitle = () => {
+    return (
+        <div className="title">
+            <h1>Lambda School</h1>
+            <h2>@Lambda School</h2>
+            <h2>26 Jan</h2>
+        </div>
+    )
+}
+
+export default HeaderTitle
