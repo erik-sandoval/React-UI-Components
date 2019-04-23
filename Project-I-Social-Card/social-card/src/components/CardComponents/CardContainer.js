5a439413dc58821a732906f520eca53faa822f34
erik-sandoval <erik.sandoval@icloud.com>
2019-04-22 22:00:32 -0700

    added surrounding link, moment.js, and better line up

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,8 +6,10 @@ import CardContent from './CardContent';
 const CardContainer = () => {
     return (
         <div className="card-container">
-            <CardBanner />
-            <CardContent />
+            <a href="https://www.reactjs.org" target="_blank" rel="noopener noreferrer">
+                <CardBanner />
+                <CardContent />
+            </a>
         </div>
     )
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import moment from 'moment'
 
 const HeaderTitle = () => {
     return (
         <div className="title">
             <h1>Lambda School</h1>
-            <h2>@Lambda School</h2>
-            <h2>• 26 Jan</h2>
+            <h2>@LambdaSchool</h2>
+            <h2> • {moment().format("YY MMM")}</h2>
         </div>
     )
 }
